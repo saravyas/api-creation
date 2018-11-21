3130f589ca65044162b66ef8fa78c39c5d2523b9
saravyas <saravyas007@gmail.com>
2018-11-21 18:51:19 +0530

    logger added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/index";
+import logger from "morgan";
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
