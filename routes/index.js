6eb2a79ea65b89bb5cbf651892e40777c7b3adf2
saravyas <saravyas007@gmail.com>
2018-11-23 10:06:34 +0530

    change in server file

--- routes/index.js
+++ routes/index.js
@@ -1,95 +1,46 @@
-import express from "express";
-import mongodata from "../models/api";
+var express = require("express");
+var router = express.Router();
+var User = require("../models/api");
 
-const router = express.Router();
-
-router
-  .route("/")
-  .get(function(req, res) {
-    var resposne = {};
-    mongodata.find({}, function(err, data) {
-      if (err) {
-        response = { error: true, msg: "error got while performing Operation" };
-      } else {
-        response = { error: false, msg: data };
-      }
-      res.json(data);
-      console.log(data);
-    });
-  })
-  .post(function(req, res) {
-    var db = new mongodata();
-    var response = {};
-    db.username = req.body.username;
-    db.password = require("crypto")
-      .createHash("sha1")
-      .update(req.body.password)
-      .digest("base64");
-    console.log(db);
-    db.save(function(err, data) {
-      if (err) {
-        response = { error: true };
-      } else {
-        response = { error: false };
-      }
-      res.json(data);
-    });
+/* GET ALL BOOKS */
+router.get("/", function(req, res, next) {
+  User.find(function(err, products) {
+    if (err) return next(err);
+    res.json(products);
   });
+});
 
-router
-  .route("/:id")
-  .get(function(req, res) {
-    var response = {};
-    //			mongodata.findOne({username: req.params.username},function(err,data){
-    mongodata.findById(req.params.id, function(err, data) {
-      if (err) {
-        reponse = { err: true, message: "Error fetching" };
-      } else {
-        response = { err: false, message: data };
-      }
-      res.json(response);
-      console.log(response);
-    });
-  })
-  .put(function(req, res) {
-    var response = {};
-    mongodata.findById(req.params.id, function(err, data) {
-      if (err) {
-        response = { error: true, message: "error while fetching data" };
-      } else {
-        if (req.body.username !== undefined) {
-          data.username = req.body.username;
-        }
-        if (req.body.password !== undefined) {
-          data.password = req.body.password;
-        }
-        data.save(function(err) {
-          if (err) {
-            response = { err: true, message: error };
-          } else {
-            response = { error: false, message: req.params.id };
-          }
-          res.json(data);
-        });
-      }
-    });
-  })
-  .delete(function(req, res) {
-    var response = {};
-    mongodata.findById(req.params.id, function(err, data) {
-      if (err) {
-        response = { error: true, message: "error" };
-      } else {
-        mongodata.remove({ _id: req.params.id }, function(err) {
-          if (err) {
-            response = { error: true, message: "error while deleting" };
-          } else {
-            response = { error: false, message: data };
-          }
-          res.json(data);
-        });
-      }
-    });
+/* GET SINGLE BOOK BY ID */
+router.get("/:id", function(req, res, next) {
+  User.findById(req.params.id, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
   });
+});
+
+/* SAVE BOOK */
+router.post("/", function(req, res, next) {
+  User.create(req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* UPDATE BOOK */
+router.put("/:id", function(req, res, next) {
+  User.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE BOOK */
+router.delete("/:id", function(req, res, next) {
+  User.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
 
 module.exports = router;
